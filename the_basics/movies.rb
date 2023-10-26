3f729e1b49d52c0f0f89efd818bafd4e675ee62e
Ben Williams <benjaminwilliamssda@gmail.com>
2023-10-25 17:48:48 -1000

    newest exercises

---
+++ the_basics/movies.rb
@@ -0,0 +1,11 @@
+movies = { :jaws => 1975, 
+:anchorman => 2004,
+:bam => 2013,
+:yam => 2001,
+:hooha => 1981 }
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:bam]
+puts movies[:yam]
+puts movies[:hooha]
