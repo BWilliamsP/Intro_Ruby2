7732696588d17edf13f70fe3c9240eba7ffc3385
Ben Williams <benjaminwilliamssda@gmail.com>
2023-10-26 18:13:21 -1000

    adding methods

---
+++ methods/greeting.rb
@@ -0,0 +1,5 @@
+def greeting(name)
+    "Hello " + name + " nice to see you"
+end
+
+puts greeting("Ben")
---
+++ methods/say.rb
@@ -0,0 +1,10 @@
+def say(words="Aloha")
+    puts words + "."
+end
+
+say()
+say("hello")
+say("hi")
+say("how are you")
+
+say "whats up"
